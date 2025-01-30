092ec96ff96ae9a6ea2646491f35ec46fbbbbc5f
Giorgio Acquati <giorgio.acquati@mia-platform.eu>
2025-01-30 23:53:35 +0100

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export", // Enables static export
+  basePath: "/fc-cm-web-parser", // Set this to your GitHub repo name
+  images: {
+    unoptimized: true, // Disable image optimization since it's not supported on GitHub Pages
+  },
 };
 
 export default nextConfig;
